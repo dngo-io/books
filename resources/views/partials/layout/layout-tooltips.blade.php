337e88baa4f58a5ff78bd6eae457804c97f473db
frnxstd <burakalbayrak@live.com>
2018-02-24 15:18:42 +0300

    tooltips partial

---
+++ resources/views/partials/layout/layout-tooltips.blade.php
@@ -0,0 +1,31 @@
+<!-- begin::Quick Nav -->
+<ul class="m-nav-sticky" style="margin-top: 30px;">
+    <!--
+    <li class="m-nav-sticky__item" data-toggle="m-tooltip" title="Showcase" data-placement="left">
+        <a href="">
+            <i class="la la-eye"></i>
+        </a>
+    </li>
+    <li class="m-nav-sticky__item" data-toggle="m-tooltip" title="Pre-sale Chat" data-placement="left">
+        <a href="" >
+            <i class="la la-comments-o"></i>
+        </a>
+    </li>
+    -->
+    <li class="m-nav-sticky__item" data-toggle="m-tooltip" title="Purchase" data-placement="left">
+        <a href="https://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes" target="_blank">
+            <i class="la la-cart-arrow-down"></i>
+        </a>
+    </li>
+    <li class="m-nav-sticky__item" data-toggle="m-tooltip" title="Documentation" data-placement="left">
+        <a href="https://keenthemes.com/metronic/documentation.html" target="_blank">
+            <i class="la la-code-fork"></i>
+        </a>
+    </li>
+    <li class="m-nav-sticky__item" data-toggle="m-tooltip" title="Support" data-placement="left">
+        <a href="https://keenthemes.com/forums/forum/support/metronic5/" target="_blank">
+            <i class="la la-life-ring"></i>
+        </a>
+    </li>
+</ul>
+<!-- begin::Quick Nav -->
