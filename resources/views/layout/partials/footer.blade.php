3d8d88c1710eec9ec4b4f5e91b9f3045ffee697b
frnxstd <burakalbayrak@live.com>
2018-02-25 15:21:53 +0300

    footer

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -3,46 +3,34 @@
     <div class="m-container m-container--responsive m-container--xxl m-container--full-height">
         <div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
             <div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
-							<span class="m-footer__copyright">
-								2017 &copy; Metronic theme by
-								<a href="https://keenthemes.com" class="m-link">
-									Keenthemes
-								</a>
-							</span>
+                <span class="m-footer__copyright">
+                    {{ date("Y") }} &copy; {{ config("app.name") }} by
+                    <a href="{{ url("http://fornaxstudio.com") }}" class="m-link">
+                        Fornax Studio
+                    </a>
+                </span>
             </div>
             <div class="m-stack__item m-stack__item--right m-stack__item--middle m-stack__item--first">
                 <ul class="m-footer__nav m-nav m-nav--inline m--pull-right">
                     <li class="m-nav__item">
                         <a href="#" class="m-nav__link">
-										<span class="m-nav__link-text">
-											About
-										</span>
-                        </a>
-                    </li>
-                    <li class="m-nav__item">
-                        <a href="#"  class="m-nav__link">
-										<span class="m-nav__link-text">
-											Privacy
-										</span>
+                            <span class="m-nav__link-text">
+                                About
+                            </span>
                         </a>
                     </li>
                     <li class="m-nav__item">
                         <a href="#" class="m-nav__link">
-										<span class="m-nav__link-text">
-											T&C
-										</span>
+                            <span class="m-nav__link-text">
+                                Rules
+                            </span>
                         </a>
                     </li>
                     <li class="m-nav__item">
                         <a href="#" class="m-nav__link">
-										<span class="m-nav__link-text">
-											Purchase
-										</span>
-                        </a>
-                    </li>
-                    <li class="m-nav__item m-nav__item--last">
-                        <a href="#" class="m-nav__link" data-toggle="m-tooltip" title="Support Center" data-placement="left">
-                            <i class="m-nav__link-icon flaticon-info m--icon-font-size-lg3"></i>
+                            <span class="m-nav__link-text">
+                                How to Contribute?
+                            </span>
                         </a>
                     </li>
                 </ul>
