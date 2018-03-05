cff319ee2212aa5c6951356741fca0c1d93b41ac
frnxstd <burakalbayrak@live.com>
2018-03-05 11:40:14 +0300

    footer watermark

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -4,10 +4,7 @@
         <div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
             <div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
                 <span class="m-footer__copyright">
-                    {{ date("Y") }} &copy; {{ config("app.name") }} by
-                    <a href="{{ url("http://fornaxstudio.com") }}" class="m-link">
-                        Fornax Studio
-                    </a>
+                    {{ date("Y") }} &copy; {{ config("app.name") }}
                 </span>
             </div>
             <div class="m-stack__item m-stack__item--right m-stack__item--middle m-stack__item--first">
