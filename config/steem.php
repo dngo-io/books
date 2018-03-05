b1d4a4710227d8aa5cf7c4db95cf0ff96cad91f0
frnxstd <burakalbayrak@live.com>
2018-03-05 22:58:33 +0300

    steem node conf

--- config/steem.php
+++ config/steem.php
@@ -9,6 +9,16 @@ return [
     |
     */
 
+    'node' => env('STEEM_NODE', 'https://steemd-int.steemit.com'),
+    /**
+     * ALTERNATIVES
+     *
+     * - https://gtg.steem.house:8090/
+     * - https://steemd.steemitdev.com/
+     * - https://steemd-int.steemit.com/
+     * - https://seed.bitcoiner.me/
+     * - https://steemd.privex.io/
+     */
     'account' => env('STEEM_ACCOUNT'),
     'about' => env('STEEM_ABOUT'),
     'rules' => env('STEEM_RULES'),
