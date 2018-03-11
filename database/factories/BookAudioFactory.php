80c2e5ff3b73a6bf05b007962c0cefc71c59ecef
cagri@fornaxstudio.com <selam@bencagri.com>
2018-03-11 17:19:05 +0300

    BookFactory user and book fix

--- database/factories/BookAudioFactory.php
+++ database/factories/BookAudioFactory.php
@@ -6,9 +6,13 @@ $factory->define(\App\Entities\BookAudio::class, function (Faker $faker) {
 
     $languages = ['en','fr','tr','de','sp'];
     $randLang = mt_rand(0,count($languages)-1);
+
+    $user = app("em")->getRepository(App\Entities\User::class)->find(1);
+    $book = app("em")->getRepository(App\Entities\Book::class)->find(mt_rand(1,500));
+
     return [
-        'user_id' => 1,
-        'book_id' => mt_rand(1,500),
+        'user' => $user,
+        'book' => $book,
         'name' => $faker->sentence(mt_rand(2,5)),
         'body' => $faker->text(),
         'chapter' => $faker->numberBetween(10,20),
