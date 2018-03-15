c4259d8eca9d3127b1f20a65e807c8087b4546d1
frnxstd <burakalbayrak@live.com>
2018-03-15 16:10:39 +0300

    Google Analytics only available on Prod

--- resources/views/layout/metronic.blade.php
+++ resources/views/layout/metronic.blade.php
@@ -79,10 +79,12 @@ m-aside-left--offcanvas-default m-aside-left--enabled m-aside-left--fixed m-asid
         @yield("script")
     });
 </script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114999163-1"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-114999163-1');
-</script>
+@if(config('env') == 'production')
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114999163-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-114999163-1');
+    </script>
+@endif
 <!-- end::Page Loader -->
 </body>
 <!-- end::Body -->
