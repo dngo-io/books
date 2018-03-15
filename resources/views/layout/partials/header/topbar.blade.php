800bb2f533ce6da7755b812b2ad301767d7486d0
frnxstd <burakalbayrak@live.com>
2018-03-15 12:54:04 +0300

    topbar moderation link

--- resources/views/layout/partials/header/topbar.blade.php
+++ resources/views/layout/partials/header/topbar.blade.php
@@ -10,7 +10,7 @@
                 <a href="#" class="m-nav__link m-dropdown__toggle">
                     <span class="m-nav__link-icon">
                         <span class="m-nav__link-icon-wrapper">
-                            <i class="flaticon-search-1"></i>
+                            <i class="fa fa-search"></i>
                         </span>
                     </span>
                 </a>
@@ -39,67 +39,18 @@
                     </div>
                 </div>
             </li>
-            <li class="d-none m-nav__item m-nav__item--danger m-dropdown m-dropdown--skin-light m-dropdown--large m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push m-dropdown--mobile-full-width m-dropdown--skin-light"
-                data-dropdown-toggle="click">
-                <a href="#" class="m-nav__link m-dropdown__toggle">
+            @if (Auth::user()->checkRole('moderator'))
+            <li class="m-nav__item m-nav__item--danger">
+                <a href="{{ url("/moderation") }}" class="m-nav__link">
                     <span class="m-nav__link-badge m-badge m-badge--dot m-badge--info m--hide"></span>
                     <span class="m-nav__link-icon">
                         <span class="m-nav__link-icon-wrapper">
-                            <i class="flaticon-share"></i>
+                            <i class="fa fa-shield"></i>
                         </span>
                     </span>
                 </a>
-                <div class="m-dropdown__wrapper">
-                    <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
-                    <div class="m-dropdown__inner">
-                        <div class="m-dropdown__header m--align-center">
-                            <span class="m-dropdown__header-title">
-                                Quick Actions
-                            </span>
-                            <span class="m-dropdown__header-subtitle">
-                                Shortcuts
-                            </span>
-                        </div>
-                        <div class="m-dropdown__body m-dropdown__body--paddingless">
-                            <div class="m-dropdown__content">
-                                <div class="m-scrollable" data-scrollable="false" data-max-height="380"
-                                     data-mobile-max-height="200">
-                                    <div class="m-nav-grid m-nav-grid--skin-light">
-                                        <div class="m-nav-grid__row">
-                                            <a href="#" class="m-nav-grid__item">
-                                                <i class="m-nav-grid__icon flaticon-file"></i>
-                                                <span class="m-nav-grid__text">
-                                                    Generate Report
-                                                </span>
-                                            </a>
-                                            <a href="#" class="m-nav-grid__item">
-                                                <i class="m-nav-grid__icon flaticon-time"></i>
-                                                <span class="m-nav-grid__text">
-                                                    Add New Event
-                                                </span>
-                                            </a>
-                                        </div>
-                                        <div class="m-nav-grid__row">
-                                            <a href="#" class="m-nav-grid__item">
-                                                <i class="m-nav-grid__icon flaticon-folder"></i>
-                                                <span class="m-nav-grid__text">
-                                                    Create New Task
-                                                </span>
-                                            </a>
-                                            <a href="#" class="m-nav-grid__item">
-                                                <i class="m-nav-grid__icon flaticon-clipboard"></i>
-                                                <span class="m-nav-grid__text">
-                                                    Completed Tasks
-                                                </span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </li>
+            @endif
             <li class="m-nav__item m-dropdown m-dropdown--medium m-dropdown--arrow  m-dropdown--align-right m-dropdown--mobile-full-width m-dropdown--skin-light" data-dropdown-toggle="click">
                 <a href="#" class="m-nav__link m-dropdown__toggle">
                     <span class="m-topbar__username m--hidden-mobile">
@@ -152,16 +103,6 @@
                                             </span>
                                         </a>
                                     </li>
-                                    @if (Auth::user()->checkRole('moderator'))
-                                    <li class="m-nav__item">
-                                        <a href="{{ url("/moderator/") }}" class="m-nav__link">
-                                            <i class="m-nav__link-icon fa fa-cog"></i>
-                                            <span class="m-nav__link-text">
-                                                Moderator
-                                            </span>
-                                        </a>
-                                    </li>
-                                    @endif
                                     <li class="m-nav__item">
                                         <a href="{{ url("https://steemit.com/@".Auth::user()->getAccount()."/settings") }}" class="m-nav__link">
                                             <i class="m-nav__link-icon fa fa-cog"></i>
