2626008fa8de113cfa293fa122035944bc642e2c
frnxstd <burakalbayrak@live.com>
2018-03-16 13:51:51 +0300

    contribution page tiny display touch

--- resources/views/audio-create.blade.php
+++ resources/views/audio-create.blade.php
@@ -40,13 +40,7 @@
                     page: params.page
                 };
             },
-            processResults: function(data, params) {
-                // parse the results into the format expected by Select2
-                // since we are using custom formatting functions we do not need to
-                // alter the remote JSON data, except to indicate that infinite
-                // scrolling can be used
-                params.page = params.page || 1;
-
+            processResults: function(data) {
                 return {
                     results: data
                 };
@@ -84,117 +78,105 @@
                     <div class="m-portlet__body">
                         <div class="row">
                             <div class="col-xl-10 offset-xl-1">
-                                <div class="m-form__section m-form__section--first">
-                                    <div class="form-group m-form__group row">
-                                        <label class="col-xl-3 col-lg-3 col-form-label">* Voiced Book:</label>
-                                        <div class="col-xl-9 col-lg-9">
-                                            <select class="form-control m-select2" id="book" name="param">
-                                                <option></option>
-                                            </select>
-                                            <span class="m-form__help">Please choose a book</span>
-                                        </div>
+                                <div class="form-group m-form__group row">
+                                    <label class="col-xl-3 col-lg-3 col-form-label">* Book:</label>
+                                    <div class="col-xl-9 col-lg-9">
+                                        <select class="form-control m-select2" id="book" name="param">
+                                            <option></option>
+                                        </select>
+                                        <span class="m-form__help">Please choose a book</span>
                                     </div>
                                 </div>
-                                <div class="m-separator m-separator--dashed m-separator--lg"></div>
-
-                                <div class="m-form__section">
-                                    <div class="m-form__heading">
-                                        <h3 class="m-form__heading-title">
-                                            Proof of Work
-                                            <i data-toggle="m-tooltip" data-width="auto" class="m-form__heading-help-icon flaticon-info" title="The form displayed below will be posted as Steemit post. Please make sure that it proves your work."></i>
-                                        </h3>
+                                <div class="form-group m-form__group row">
+                                    <label class="col-xl-3 col-lg-3 col-form-label">* Post Title:</label>
+                                    <div class="col-xl-9 col-lg-9">
+                                        <input type="text" name="title" class="form-control m-input" placeholder="" value="The Tale of Two Cities - Chapter 4 Audio Book">
+                                        <span class="m-form__help">The title will be displayed as Steemit post title.</span>
                                     </div>
-                                    <div class="form-group m-form__group row">
-                                        <label class="col-xl-3 col-lg-3 col-form-label">* Post Title:</label>
-                                        <div class="col-xl-9 col-lg-9">
-                                            <input type="text" name="title" class="form-control m-input" placeholder="" value="The Tale of Two Cities - Chapter 4 Audio Book">
-                                            <span class="m-form__help">The title will be displayed as Steemit post title.</span>
-                                        </div>
+                                </div>
+                                <div class="form-group m-form__group row">
+                                    <label class="col-xl-3 col-lg-3 col-form-label">* Post Body:</label>
+                                    <div class="col-xl-9 col-lg-9">
+                                        <textarea name="content" class="form-control" data-provide="markdown" rows="15"></textarea>
+                                        <span class="m-form__help">Appartment, suite, unit, building, floor, etc</span>
                                     </div>
-                                    <div class="form-group m-form__group row">
-                                        <label class="col-xl-3 col-lg-3 col-form-label">* Post Body:</label>
-                                        <div class="col-xl-9 col-lg-9">
-                                            <textarea name="content" class="form-control" data-provide="markdown" rows="15"></textarea>
-                                            <span class="m-form__help">Appartment, suite, unit, building, floor, etc</span>
+                                </div>
+                                <div class="form-group m-form__group row">
+                                    <label class="col-xl-3 col-lg-3 col-form-label" for="audio">* Audio:</label>
+                                    <div class="col-xl-9 col-lg-9">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" name="audio" id="audio">
+                                            <label class="custom-file-label" for="customFile">Choose file</label>
                                         </div>
+                                        <span class="m-form__help">Browse your local device and choose a file to upload</span>
                                     </div>
-                                    <div class="form-group m-form__group row">
-                                        <label class="col-xl-3 col-lg-3 col-form-label" for="audio">* Audio:</label>
-                                        <div class="col-xl-9 col-lg-9">
-                                            <div class="custom-file">
-                                                <input type="file" class="custom-file-input" name="audio" id="audio">
-                                                <label class="custom-file-label" for="customFile">Choose file</label>
-                                            </div>
-                                            <span class="m-form__help">Browse your local device and choose a file to upload</span>
-                                        </div>
-                                    </div>
-                                    <div class="form-group m-form__group row">
-                                        <label class="col-xl-3 col-lg-3 col-form-label">* Tags:</label>
-                                        <div class="col-xl-9 col-lg-9">
-                                            <select class="form-control m-select2" id="tags" name="tags" multiple>
-                                                <option></option>
-                                                <optgroup label="Alaskan/Hawaiian Time Zone">
-                                                    <option value="AK">Alaska</option>
-                                                    <option value="HI">Hawaii</option>
-                                                </optgroup>
-                                                <optgroup label="Pacific Time Zone">
-                                                    <option value="CA">California</option>
-                                                    <option value="NV" selected>Nevada</option>
-                                                    <option value="OR">Oregon</option>
-                                                    <option value="WA">Washington</option>
-                                                </optgroup>
-                                                <optgroup label="Mountain Time Zone">
-                                                    <option value="AZ">Arizona</option>
-                                                    <option value="CO">Colorado</option>
-                                                    <option value="ID">Idaho</option>
-                                                    <option value="MT">Montana</option>
-                                                    <option value="NE">Nebraska</option>
-                                                    <option value="NM">New Mexico</option>
-                                                    <option value="ND">North Dakota</option>
-                                                    <option value="UT">Utah</option>
-                                                    <option value="WY">Wyoming</option>
-                                                </optgroup>
-                                                <optgroup label="Central Time Zone">
-                                                    <option value="AL">Alabama</option>
-                                                    <option value="AR">Arkansas</option>
-                                                    <option value="IL">Illinois</option>
-                                                    <option value="IA">Iowa</option>
-                                                    <option value="KS">Kansas</option>
-                                                    <option value="KY">Kentucky</option>
-                                                    <option value="LA">Louisiana</option>
-                                                    <option value="MN">Minnesota</option>
-                                                    <option value="MS">Mississippi</option>
-                                                    <option value="MO">Missouri</option>
-                                                    <option value="OK">Oklahoma</option>
-                                                    <option value="SD">South Dakota</option>
-                                                    <option value="TX">Texas</option>
-                                                    <option value="TN">Tennessee</option>
-                                                    <option value="WI">Wisconsin</option>
-                                                </optgroup>
-                                                <optgroup label="Eastern Time Zone">
-                                                    <option value="CT">Connecticut</option>
-                                                    <option value="DE">Delaware</option>
-                                                    <option value="FL">Florida</option>
-                                                    <option value="GA">Georgia</option>
-                                                    <option value="IN">Indiana</option>
-                                                    <option value="ME">Maine</option>
-                                                    <option value="MD">Maryland</option>
-                                                    <option value="MA">Massachusetts</option>
-                                                    <option value="MI">Michigan</option>
-                                                    <option value="NH">New Hampshire</option>
-                                                    <option value="NJ">New Jersey</option>
-                                                    <option value="NY">New York</option>
-                                                    <option value="NC">North Carolina</option>
-                                                    <option value="OH">Ohio</option>
-                                                    <option value="PA">Pennsylvania</option>
-                                                    <option value="RI">Rhode Island</option>
-                                                    <option value="SC">South Carolina</option>
-                                                    <option value="VT">Vermont</option>
-                                                    <option value="VA">Virginia</option>
-                                                    <option value="WV">West Virginia</option>
-                                                </optgroup>
-                                            </select>
-                                        </div>
+                                </div>
+                                <div class="form-group m-form__group row">
+                                    <label class="col-xl-3 col-lg-3 col-form-label">* Tags:</label>
+                                    <div class="col-xl-9 col-lg-9">
+                                        <select class="form-control m-select2" id="tags" name="tags" multiple>
+                                            <option></option>
+                                            <optgroup label="Alaskan/Hawaiian Time Zone">
+                                                <option value="AK">Alaska</option>
+                                                <option value="HI">Hawaii</option>
+                                            </optgroup>
+                                            <optgroup label="Pacific Time Zone">
+                                                <option value="CA">California</option>
+                                                <option value="NV" selected>Nevada</option>
+                                                <option value="OR">Oregon</option>
+                                                <option value="WA">Washington</option>
+                                            </optgroup>
+                                            <optgroup label="Mountain Time Zone">
+                                                <option value="AZ">Arizona</option>
+                                                <option value="CO">Colorado</option>
+                                                <option value="ID">Idaho</option>
+                                                <option value="MT">Montana</option>
+                                                <option value="NE">Nebraska</option>
+                                                <option value="NM">New Mexico</option>
+                                                <option value="ND">North Dakota</option>
+                                                <option value="UT">Utah</option>
+                                                <option value="WY">Wyoming</option>
+                                            </optgroup>
+                                            <optgroup label="Central Time Zone">
+                                                <option value="AL">Alabama</option>
+                                                <option value="AR">Arkansas</option>
+                                                <option value="IL">Illinois</option>
+                                                <option value="IA">Iowa</option>
+                                                <option value="KS">Kansas</option>
+                                                <option value="KY">Kentucky</option>
+                                                <option value="LA">Louisiana</option>
+                                                <option value="MN">Minnesota</option>
+                                                <option value="MS">Mississippi</option>
+                                                <option value="MO">Missouri</option>
+                                                <option value="OK">Oklahoma</option>
+                                                <option value="SD">South Dakota</option>
+                                                <option value="TX">Texas</option>
+                                                <option value="TN">Tennessee</option>
+                                                <option value="WI">Wisconsin</option>
+                                            </optgroup>
+                                            <optgroup label="Eastern Time Zone">
+                                                <option value="CT">Connecticut</option>
+                                                <option value="DE">Delaware</option>
+                                                <option value="FL">Florida</option>
+                                                <option value="GA">Georgia</option>
+                                                <option value="IN">Indiana</option>
+                                                <option value="ME">Maine</option>
+                                                <option value="MD">Maryland</option>
+                                                <option value="MA">Massachusetts</option>
+                                                <option value="MI">Michigan</option>
+                                                <option value="NH">New Hampshire</option>
+                                                <option value="NJ">New Jersey</option>
+                                                <option value="NY">New York</option>
+                                                <option value="NC">North Carolina</option>
+                                                <option value="OH">Ohio</option>
+                                                <option value="PA">Pennsylvania</option>
+                                                <option value="RI">Rhode Island</option>
+                                                <option value="SC">South Carolina</option>
+                                                <option value="VT">Vermont</option>
+                                                <option value="VA">Virginia</option>
+                                                <option value="WV">West Virginia</option>
+                                            </optgroup>
+                                        </select>
                                     </div>
                                 </div>
                             </div>
