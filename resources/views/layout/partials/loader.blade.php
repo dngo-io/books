9b22b881f0064d4c1d0b191033350474506a7d54
frnxstd <burakalbayrak@live.com>
2018-03-16 13:07:39 +0300

    loading blade

--- resources/views/layout/partials/loader.blade.php
+++ resources/views/layout/partials/loader.blade.php
@@ -1,12 +1,7 @@
 <!-- begin::Page loader -->
 <div class="m-page-loader m-page-loader--base">
     <div class="m-blockui">
-        <span class="d-none">
-            Please wait...
-        </span>
-        <span>
-            <div class="m-loader m-loader--brand"></div>
-        </span>
+        <div class="m-loader m-loader--brand"></div>
     </div>
 </div>
 <!-- end::Page Loader -->
