f7d6da779244c3281fcfdca195a2253c9045998b
frnxstd <burakalbayrak@live.com>
2018-03-17 14:05:38 +0300

    audio-create blade

--- resources/views/audio-create.blade.php
+++ resources/views/audio-create.blade.php
@@ -55,9 +55,28 @@
         templateSelection: formatRepoSelection // omitted for brevity, see the source of this page
     });
 
-    $('#tags').select2({
+    $("#tags").select2({
         placeholder: "Select an option",
-        maximumSelectionLength: 2
+        maximumSelectionLength: 2,
+        allowClear: true,
+        tags: true,
+        ajax: {
+            url: "{{ url("action/audio-tags") }}",
+            dataType: 'json',
+            delay: 500,
+            data: function(params) {
+                return {
+                    name: params.term, // search term
+                    page: params.page
+                };
+            },
+            processResults: function(data) {
+                return {
+                    results: data
+                };
+            },
+            cache: true
+        }
     });
 @endsection
 @section("content")
@@ -114,69 +133,7 @@
                                 <div class="form-group m-form__group row">
                                     <label class="col-xl-3 col-lg-3 col-form-label">* Tags:</label>
                                     <div class="col-xl-9 col-lg-9">
-                                        <select class="form-control m-select2" id="tags" name="tags" multiple>
-                                            <option></option>
-                                            <optgroup label="Alaskan/Hawaiian Time Zone">
-                                                <option value="AK">Alaska</option>
-                                                <option value="HI">Hawaii</option>
-                                            </optgroup>
-                                            <optgroup label="Pacific Time Zone">
-                                                <option value="CA">California</option>
-                                                <option value="NV" selected>Nevada</option>
-                                                <option value="OR">Oregon</option>
-                                                <option value="WA">Washington</option>
-                                            </optgroup>
-                                            <optgroup label="Mountain Time Zone">
-                                                <option value="AZ">Arizona</option>
-                                                <option value="CO">Colorado</option>
-                                                <option value="ID">Idaho</option>
-                                                <option value="MT">Montana</option>
-                                                <option value="NE">Nebraska</option>
-                                                <option value="NM">New Mexico</option>
-                                                <option value="ND">North Dakota</option>
-                                                <option value="UT">Utah</option>
-                                                <option value="WY">Wyoming</option>
-                                            </optgroup>
-                                            <optgroup label="Central Time Zone">
-                                                <option value="AL">Alabama</option>
-                                                <option value="AR">Arkansas</option>
-                                                <option value="IL">Illinois</option>
-                                                <option value="IA">Iowa</option>
-                                                <option value="KS">Kansas</option>
-                                                <option value="KY">Kentucky</option>
-                                                <option value="LA">Louisiana</option>
-                                                <option value="MN">Minnesota</option>
-                                                <option value="MS">Mississippi</option>
-                                                <option value="MO">Missouri</option>
-                                                <option value="OK">Oklahoma</option>
-                                                <option value="SD">South Dakota</option>
-                                                <option value="TX">Texas</option>
-                                                <option value="TN">Tennessee</option>
-                                                <option value="WI">Wisconsin</option>
-                                            </optgroup>
-                                            <optgroup label="Eastern Time Zone">
-                                                <option value="CT">Connecticut</option>
-                                                <option value="DE">Delaware</option>
-                                                <option value="FL">Florida</option>
-                                                <option value="GA">Georgia</option>
-                                                <option value="IN">Indiana</option>
-                                                <option value="ME">Maine</option>
-                                                <option value="MD">Maryland</option>
-                                                <option value="MA">Massachusetts</option>
-                                                <option value="MI">Michigan</option>
-                                                <option value="NH">New Hampshire</option>
-                                                <option value="NJ">New Jersey</option>
-                                                <option value="NY">New York</option>
-                                                <option value="NC">North Carolina</option>
-                                                <option value="OH">Ohio</option>
-                                                <option value="PA">Pennsylvania</option>
-                                                <option value="RI">Rhode Island</option>
-                                                <option value="SC">South Carolina</option>
-                                                <option value="VT">Vermont</option>
-                                                <option value="VA">Virginia</option>
-                                                <option value="WV">West Virginia</option>
-                                            </optgroup>
-                                        </select>
+                                        <select class="form-control m-select2" id="tags" name="tags" multiple></select>
                                     </div>
                                 </div>
                             </div>
