b145921f41d5fde4dece71a9e4f49ed45b4f1889
frnxstd <burak@myself.com>
2018-03-23 09:23:56 +0300

    get all content replies

--- steem-api/src/API/Post.php
+++ steem-api/src/API/Post.php
@@ -7,21 +7,64 @@ class Post extends Query
     /**
      * Get content
      *
-     * @param string $account Author's account name
-     * @param string $permlink Post's permalink
+     * @param string $author    Author's account name
+     * @param string $permalink Post's permalink
      * @return array
      */
-    public function getContent(string $author, string $permlink)
+    public function getContent(string $author, string $permalink)
     {
         $request = [
             'route' => 'get_content',
             'query' =>
                 [
                     'author'   => $author,
-                    'permlink' => $permlink,
+                    'permlink' => $permalink,
                 ]
         ];
 
         return parent::call($request);
+    }
+
+    /**
+     * Get content replies
+     *
+     * @param string $author    Author's account name
+     * @param string $permalink Post's permalink
+     * @return array
+     */
+    public function getContentReplies(string $author, string $permalink)
+    {
+        $request = [
+            'route' => 'get_content_replies',
+            'query' =>
+                [
+                    'author'   => $author,
+                    'permlink' => $permalink,
+                ]
+        ];
+
+        return parent::call($request);
+    }
+
+    /**
+     * Get all content replies
+     *
+     * @param string $author    Author's account name
+     * @param string $permalink Post's permalink
+     * @return array
+     */
+    public function getContentAllReplies(string $author, string $permalink)
+    {
+        $request = $this->getContentReplies($author, $permalink);
+
+        foreach ($request as $key => $item)
+        {
+            if($item['children'] > 0)
+                $request[$key]['replies'] = $this->getContentAllReplies($item['author'], $item['permlink']);
+            else
+                $request[$key]['replies'] = [];
+        }
+
+        return $request;
     }
 }
