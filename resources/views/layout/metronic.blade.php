c395007f63231b263f5c4d4a8bc6cb60b42b7fca
frnxstd <burak@myself.com>
2018-03-24 18:53:03 +0300

    moderation js auth isset control

--- resources/views/layout/metronic.blade.php
+++ resources/views/layout/metronic.blade.php
@@ -66,7 +66,7 @@ m-aside-left--offcanvas-default m-aside-left--enabled m-aside-left--fixed m-asid
 <script src="{{ asset("assets/custom/plugins/amplitudejs/examples/resources/js/foundation.min.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/plugins/amplitudejs/examples/blue-playlist/js/functions.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/js/custom.js") }}" type="text/javascript"></script>
-@if (Auth::user()->checkRole('moderator'))
+@if (!Auth::guest() && Auth::user()->checkRole('moderator'))
 <script src="{{ asset("assets/custom/js/moderation.js") }}" type="text/javascript"></script>
 @endif
 <!--end::Page Snippets -->
