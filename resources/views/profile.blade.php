32075ac4b54b3e8e1cdedc3dbad081ad78fc4e87
frnxstd <burak@myself.com>
2018-03-25 11:27:27 +0300

    profile blade - author helper added

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -74,7 +74,7 @@
                     <div class="m-portlet__head-caption">
                         <div class="m-portlet__head-title">
                             <h3 class="m-portlet__head-text">
-                                {{ $user->getAccount() }}
+                                {{ author($user) }}
                             </h3>
                         </div>
                     </div>
