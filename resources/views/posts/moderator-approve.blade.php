d88452af72819f40c80ef89fa1f7a7931afd339a
frnxstd <burak@myself.com>
2018-03-28 00:47:54 +0300

    moderator blades

--- resources/views/posts/moderator-approve.blade.php
+++ resources/views/posts/moderator-approve.blade.php
@@ -1,8 +1,6 @@
 Thank you for the contribution. It has been approved.
 <br>
-<br>
 {!! $comment !!}
-<br>
 <br>
 You can contact us on Discord, join us!
 <br>
--- resources/views/posts/moderator-reject.blade.php
+++ resources/views/posts/moderator-reject.blade.php
@@ -1,9 +1,7 @@
 Your contribution cannot be approved because it does not follow the
 <a href="{{ config("steem.rules") }}">{{ config("app.name") }} Rules</a>.
 <br>
-<br>
 {!! $comment !!}
-<br>
 <br>
 You can contact us on Discord, join us!
 <br>
