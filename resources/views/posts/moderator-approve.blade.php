a26cfb948738fc8725efaf84b8ecd8e92c153128
frnxstd <burak@myself.com>
2018-03-27 11:29:00 +0300

    post blades

---
+++ resources/views/posts/moderator-approve.blade.php
@@ -0,0 +1,9 @@
+Thank you for the contribution. It has been approved.
+<br>
+<br>
+{!! $comment !!}
+<br>
+<br>
+You can contact us on Discord, join us!
+<br>
+**[MODERATOR]**
---
+++ resources/views/posts/signature.blade.php
@@ -0,0 +1,14 @@
+{!! $post !!}
+<hr>
+<div style="text-align: center;">
+    <div style="margin-bottom: 20px">
+        <a href="{{ url("") }}" title="{{ config("app.name") }}">
+            <img src="{{ asset("assets/custom/img/dngo-hq-logo.png") }}" alt="{{ config("app.name") }}" width="250">
+        </a>
+    </div>
+    <br>
+
+    This is signature! {{ config("app.name") }}
+
+    <br>
+</div>
