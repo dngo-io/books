b62bfb1cab24e080a3bf1983e7e3dcbecc28ea39
frnxstd <burak@myself.com>
2018-03-29 13:24:43 +0300

    comments css fixed

--- resources/views/layout/partials/comments-sub.blade.php
+++ resources/views/layout/partials/comments-sub.blade.php
@@ -4,8 +4,8 @@
     </a>
     <div class="media-body">
         <h5 class="mt-0">{{ author($_reply) }}</h5>
-        {!! parse_md($_reply['body']) !!}
-        <p>
+        <div class="comment-body">{!! parse_md($_reply['body']) !!}</div>
+        <p class="comment-body">
             <i class="fa fa-chevron-up m--font-success d-none"></i> {{ payout($_reply['pending_payout_value']) }}
             <a href="{{ url('https://steemit.com'.str_start($_reply['url'], '/')) }}" class="ml-5 m-link">Reply</a>
         </p>
