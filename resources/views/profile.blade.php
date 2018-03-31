216f2f96d0af1ff132c9c8ac3d4ccd6f40dfb1e0
frnxstd <burak@myself.com>
2018-03-31 12:13:48 +0300

    user profile - no contribution

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -87,7 +87,7 @@
                     </div>
                 </div>
                 <div class="m-portlet__body">
-                    @if($contribution == 0)
+                    @if($contribution > 0)
                     <nav aria-label="Search result pagination">
                         {{ $pagination->render('pagination::bootstrap-4') }}
                     </nav>
