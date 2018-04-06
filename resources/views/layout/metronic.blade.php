833243f5758c6a55395c0b6a71e70ce037a274f3
cagri@fornaxstudio.com <selam@bencagri.com>
2018-04-06 15:52:20 +0300

    add speech js to footer

--- resources/views/layout/metronic.blade.php
+++ resources/views/layout/metronic.blade.php
@@ -65,6 +65,7 @@ m-aside-left--offcanvas-default m-aside-left--enabled m-aside-left--fixed m-asid
 <!--end::Page Vendors -->
 <!--begin::Page Snippets -->
 <script src="{{ asset("assets/app/js/dashboard.js") }}" type="text/javascript"></script>
+<script src="{{ asset("assets/app/js/speech.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/plugins/amplitudejs/dist/amplitude.min.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/plugins/amplitudejs/examples/resources/js/foundation.min.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/plugins/amplitudejs/examples/blue-playlist/js/functions.js") }}" type="text/javascript"></script>
@@ -90,6 +91,7 @@ m-aside-left--offcanvas-default m-aside-left--enabled m-aside-left--fixed m-asid
     </script>
 @endif
 <!-- end::Page Loader -->
+    <input type="hidden" class="dngo-dynamic-speech" value="" />
 </body>
 <!-- end::Body -->
 </html>
