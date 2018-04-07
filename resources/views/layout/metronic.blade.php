7590cbee004ef5a4342ad516688cbf6758f8a61a
frnxstd <burak@myself.com>
2018-04-07 10:39:24 +0300

    metronic update

--- resources/views/layout/metronic.blade.php
+++ resources/views/layout/metronic.blade.php
@@ -58,13 +58,8 @@ m-aside-left--offcanvas-default m-aside-left--enabled m-aside-left--fixed m-asid
 @yield("page")
 <!--begin::Base Scripts -->
 <script src="{{ asset("assets/vendors/base/vendors.bundle.js") }}" type="text/javascript"></script>
-<script src="{{ asset("assets/demo/demo9/base/scripts.bundle.js") }}" type="text/javascript"></script>
+<script src="{{ asset("assets/demo/default/base/scripts.bundle.js") }}" type="text/javascript"></script>
 <!--end::Base Scripts -->
-<!--begin::Page Vendors -->
-<script src="{{ asset("assets/vendors/custom/fullcalendar/fullcalendar.bundle.js") }}" type="text/javascript"></script>
-<!--end::Page Vendors -->
-<!--begin::Page Snippets -->
-<script src="{{ asset("assets/app/js/dashboard.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/plugins/amplitudejs/dist/amplitude.min.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/plugins/amplitudejs/examples/resources/js/foundation.min.js") }}" type="text/javascript"></script>
 <script src="{{ asset("assets/custom/plugins/amplitudejs/examples/blue-playlist/js/functions.js") }}" type="text/javascript"></script>
