f546c87b2ed6bc927b00a9ecf9333084e617ef6c
frnxstd <burak@myself.com>
2018-04-10 14:10:32 +0300

    profile partials

---
+++ resources/views/layout/partials/profile/user-bar.blade.php
@@ -0,0 +1,71 @@
+<div class="m-portlet m-portlet--full-height">
+    <div class="m-portlet__body">
+        <div class="m-card-profile">
+            <div class="m-card-profile__title m--hide">
+                Profile
+            </div>
+            <div class="m-card-profile__pic">
+                <div class="m-card-profile__pic-wrapper">
+                    <img src="{{ get_steem_pp($user->getProfileImage())}}" alt="{{ author($user, false) }}">
+                </div>
+            </div>
+            <div class="m-card-profile__details">
+                <span class="m-card-profile__name">{{ $user->getName() }}</span>
+                <a href="{{ url("https://steemit.com/@{$user->getAccount()}") }}" class="m-card-profile__email m-link">{{ author($steem_data['user']) }}</a>
+            </div>
+            <br>
+            <div class="text-center">
+                {{ voting_power($steem_data['user']) }}%
+                <div class="progress m-progress--sm">
+                    <div class="progress-bar m--bg-accent" role="progressbar" style="width: {{ voting_power($steem_data['user']) }}%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+        <ul class="m-nav m-nav--hover-bg m-portlet-fit--sides">
+            <li class="m-nav__separator m-nav__separator--fit"></li>
+            <li class="m-nav__section m--hide">
+                <span class="m-nav__section-text">Section</span>
+            </li>
+            <li class="m-nav__item">
+                <a href="{{ url("https://steemit.com/@{$user->getAccount()}") }}" class="m-nav__link">
+                    <i class="m-nav__link-icon fa fa-user"></i>
+                    <span class="m-nav__link-text">Steemit Profile</span>
+                </a>
+            </li>
+            <li class="m-nav__item">
+                <a href="{{ url("https://steemit.com/@{$user->getAccount()}/followed") }}" class="m-nav__link m-tabs__item">
+                    <i class="m-nav__link-icon fa fa-arrow-left"></i>
+                    <span class="m-nav__link-text">Following</span>
+                    <span class="m-nav__link-badge">
+                                    <span class="m-badge m-badge--metal">{{ $steem_data['follows']['following_count'] }}</span>
+                                </span>
+                </a>
+            </li>
+            <li class="m-nav__item">
+                <a href="{{ url("https://steemit.com/@{$user->getAccount()}/followers") }}" class="m-nav__link m-tabs__item">
+                    <i class="m-nav__link-icon fa fa-arrow-right"></i>
+                    <span class="m-nav__link-text">Followers</span>
+                    <span class="m-nav__link-badge">
+                                    <span class="m-badge m-badge--metal">{{ $steem_data['follows']['follower_count'] }}</span>
+                                </span>
+                </a>
+            </li>
+        </ul>
+
+        <div class="m-portlet__body-separator"></div>
+
+        <div class="m-widget1 m-widget1--paddingless">
+            <div class="m-widget1__item">
+                <div class="row m-row--no-padding align-items-center">
+                    <div class="col">
+                        <h3 class="m-widget1__title">Contributions</h3>
+                        <span class="m-widget1__desc">Contributor since {{ format_date($user->getCreatedAt(), 'M, y') }}</span>
+                    </div>
+                    <div class="col m--align-right">
+                        <span class="m-widget1__number m--font-success">{{ $contribution }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,77 +3,7 @@
 @section("content")
     <div class="row">
         <div class="col-lg-4">
-            <div class="m-portlet m-portlet--full-height">
-                <div class="m-portlet__body">
-                    <div class="m-card-profile">
-                        <div class="m-card-profile__title m--hide">
-                            Profile
-                        </div>
-                        <div class="m-card-profile__pic">
-                            <div class="m-card-profile__pic-wrapper">
-                                <img src="{{ get_steem_pp($user->getProfileImage())}}" alt="{{ author($user, false) }}">
-                            </div>
-                        </div>
-                        <div class="m-card-profile__details">
-                            <span class="m-card-profile__name">{{ $user->getName() }}</span>
-                            <a href="{{ url("https://steemit.com/@{$user->getAccount()}") }}" class="m-card-profile__email m-link">{{ author($steem_data['user']) }}</a>
-                        </div>
-                        <br>
-                        <div class="text-center">
-                            {{ voting_power($steem_data['user']) }}%
-                            <div class="progress m-progress--sm">
-                                <div class="progress-bar m--bg-accent" role="progressbar" style="width: {{ voting_power($steem_data['user']) }}%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <ul class="m-nav m-nav--hover-bg m-portlet-fit--sides">
-                        <li class="m-nav__separator m-nav__separator--fit"></li>
-                        <li class="m-nav__section m--hide">
-                            <span class="m-nav__section-text">Section</span>
-                        </li>
-                        <li class="m-nav__item">
-                            <a href="{{ url("https://steemit.com/@{$user->getAccount()}") }}" class="m-nav__link">
-                                <i class="m-nav__link-icon fa fa-user"></i>
-                                <span class="m-nav__link-text">Steemit Profile</span>
-                            </a>
-                        </li>
-                        <li class="m-nav__item">
-                            <a href="{{ url("https://steemit.com/@{$user->getAccount()}/followed") }}" class="m-nav__link m-tabs__item">
-                                <i class="m-nav__link-icon fa fa-arrow-left"></i>
-                                <span class="m-nav__link-text">Following</span>
-                                <span class="m-nav__link-badge">
-                                    <span class="m-badge m-badge--metal">{{ $steem_data['follows']['following_count'] }}</span>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="m-nav__item">
-                            <a href="{{ url("https://steemit.com/@{$user->getAccount()}/followers") }}" class="m-nav__link m-tabs__item">
-                                <i class="m-nav__link-icon fa fa-arrow-right"></i>
-                                <span class="m-nav__link-text">Followers</span>
-                                <span class="m-nav__link-badge">
-                                    <span class="m-badge m-badge--metal">{{ $steem_data['follows']['follower_count'] }}</span>
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <div class="m-portlet__body-separator"></div>
-
-                    <div class="m-widget1 m-widget1--paddingless">
-                        <div class="m-widget1__item">
-                            <div class="row m-row--no-padding align-items-center">
-                                <div class="col">
-                                    <h3 class="m-widget1__title">Contributions</h3>
-                                    <span class="m-widget1__desc">Contributor since {{ format_date($user->getCreatedAt(), 'M, y') }}</span>
-                                </div>
-                                <div class="col m--align-right">
-                                    <span class="m-widget1__number m--font-success">{{ $contribution }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include("layout.partials.profile.user-bar")
         </div>
         <div class="col-lg-8">
             <div class="m-portlet m-portlet--full-height">
@@ -87,77 +17,7 @@
                     </div>
                 </div>
                 <div class="m-portlet__body">
-                    @if($contribution > 0)
-                    <nav aria-label="Search result pagination">
-                        {{ $pagination->render('pagination::bootstrap-4') }}
-                    </nav>
-
-                    <div class="m-widget5">
-                        @foreach($feed as $item)
-                            <div class="m-widget5__item">
-                                <div class="m-widget5__content">
-                                    <h4 class="m-widget5__title">
-                                        <a href="{{ url("listen/{$item->getId()}") }}" title="{{ $item->getName() }}" class="m-link">
-                                            {{ $item->getName() }}
-                                        </a>
-                                    </h4>
-                                    <span class="m-widget5__desc">
-                                        <blockquote class="blockquote blockquote-reverse">
-                                            <p class="mb-0"><i class="fa fa-book"></i> {{ $item->getBook()->getName() }}</p>
-                                            <footer class="blockquote-footer">By
-                                                <cite title="{{ $item->getBook()->getAuthor()->getName() }}">
-                                                    <a href="{{ url("author/{$item->getBook()->getAuthor()->getId()}") }}" class="m-link">
-                                                        {{ $item->getBook()->getAuthor()->getName() }}
-                                                    </a>
-                                                </cite>
-                                                in
-                                                <cite title="{{ $item->getBook()->getYear() }}">
-                                                    {{ $item->getBook()->getYear() }}
-                                                </cite>
-                                            </footer>
-                                        </blockquote>
-
-                                        {{ str_limit(strip_tags(markdown($item->getBody())), 200, '...') }}
-                                    </span>
-                                    <div class="m-widget5__info">
-                                        <span class="m-widget5__author">
-                                            by
-                                        </span>
-                                        <span class="m-widget5__info-author-name">
-                                            <a href="{{ url("user/{$item->getUser()->getAccount()}") }}" class="m-link">
-                                                {{ author($item->getUser()) }}
-                                            </a>
-                                        </span>
-                                        <span class="m-widget5__info-label">
-                                            on
-                                        </span>
-                                        <span class="m-widget5__info-date">
-                                            {{ format_date($item->getCreatedAt()) }}
-                                        </span>
-                                    </div>
-                                </div>
-                                <div class="m-widget5__stats1 text-center pl-0">
-                                    <a class="m-link" href="{{ url("book/{$item->getBook()->getId()}") }}">
-                                        <span><i class="fa fa-3x fa-book"></i></span><br>
-                                        <span>Book</span>
-                                    </a>
-                                </div>
-                                <div class="m-widget5__stats2 text-center pl-0">
-                                    <a class="m-link" href="{{ url("listen/{$item->getId()}") }}">
-                                        <span><i class="fa fa-3x fa-play"></i></span><br>
-                                        <span>Listen</span>
-                                    </a>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
-
-                    <nav aria-label="Search result pagination">
-                        {{ $pagination->render('pagination::bootstrap-4') }}
-                    </nav>
-                    @else
-                    <p class="text-center">No contribution found</p>
-                    @endif
+                    @include("layout.partials.profile.feed")
                 </div>
             </div>
         </div>
